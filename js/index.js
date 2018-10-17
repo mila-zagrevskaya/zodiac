3db30308cc3219fba1bb6e1852a81efe04dd169f
Mila-Zagrevskaya <mila.zagrevskaya@gmail.com>
2018-10-17 14:54:09 +0300

    добавлено всплывающее окно

--- js/index.js
+++ js/index.js
@@ -18,8 +18,8 @@ function movePoint () {
    var x = point.currentPosition [0]
 	 var y = point.currentPosition [1]
 	 var dir = point.currentDirection
-	 var w = window.innerWidth - 55
-	 var h = window.innerHeight - 65
+	 var w = window.innerWidth - 60
+	 var h = window.innerHeight - 80
 	 var dx = 0
 	 var dy = 0
 	 switch ( point.currentDirection ) {
@@ -54,83 +54,30 @@ function movePoint () {
 
 // _______________________
 // вывод описания знаков на экран
-var text 
 
-var request = new XMLHttpRequest()
-request.onreadystatechange = function(event){
-   if (this.readyState === 4)
-      if (this.status ===200){
-          text =  JSON.parse(this.responseText)
-          for ( var elem of text){
-              var text = document.createElement('div')
-              text.src = elem.content
-              document.body.appendChild(text)
-              text.title = elem.name
-              text.className = ""
-          }    
-      } 
+function getInform (url) {
+  var container = document.querySelector (".sign")
+   var messWin = document.createElement('div')
+    messWin.className = "textBox"
+    container.appendChild ( messWin )
+    messWin.style.display = 'none'
+    messWin.onclick = function ( event ) {
+      event.target.style.display = "none"
+    }
+
+    messWin.style.display = 'block'
+    messWin.innerHTML = getFileFromServer ( url )
+   
+  
+   function getFileFromServer ( url ) {
+    var request = new XMLHttpRequest ()
+    request.className = "text"
+    request.onload = function () {
+      messWin.innerHTML += this.responseText
+    }
+    request.open ( "GET", url )
+    request.send ()
+   }
 }
-request.open("GET","content.json")
-request.send()
-// ______________
 
-// показ сообщения с контентом
-// var messages = content.json.name
-// var messageHeaders = content.json.content
-
-// var messages = [ 
-//   "тип события ('click', 'mousover', 'mouseenter')", 
-//   "ссылка на элемент, на котором произошло событие", 
-//   "координата Х указателя мышки в момент события", 
-//   "координата Y указателя мышки в момент события"]
-
-// var messageHeaders = [
-//  "images/oven.png",
-// 	"images/telets.png",
-// 	"images/bliznetsy.png",
-// 	"images/rak.png",
-// 	"images/lev.png",
-// 	"images/deva.png",
-// 	"images/vesy.png",
-// 	"images/scorpion.png",
-// 	"images/strelets.png",
-// 	"images/kozerog.png",
-// 	"images/vodoley.png",
-// 	"images/ryby.png"
-// ]
-
-
-// function createTitle (event) {
-  
-//   if ( !messages || !messageHeaders ) return
-  
-//   event.target.style.display = "none"
-  
-//   var messWin = document.createElement('p')
-//   messWin.className = "textBox"
-//   document.body.appendChild ( messWin )
-//   messWin.style.display = 'none'
-//   messWin.onclick = function ( event ) {
-//     event.target.style.display = "none"
-//   }
-  
-//   var pictBlock = document.createElement('img')
-//   pictBlock.src = "messageHeaders [i]"
-//   document.body.appendChild(pictBlock)
-
-//   pictBlock.className = "menu"
-  
-//   for ( var i=0; i < messageHeaders.length; i++ ) {
-//     var messHead = document.createElement('h3')
-//     messHead.innerHTML = messageHeaders [i]
-//     messHead.className = "textHeader"
-//     messHead.txt = messages [i]
-//     pictBlock.appendChild ( messHead )
-    
-//     pict.onclick = function ( event ) {
-//       messWin.innerHTML = event.target.txt
-//       messWin.style.display = 'block'
-//     }
-//   }
-// }
 
